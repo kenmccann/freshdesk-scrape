6bcd079e671b4e9f07fb780fb14dc6d70c26cc35
Ken McCann <ken.mccann@aquasec.com>
2023-12-18 10:30:18 +0100

    added more intelligent rate limit handling

--- freshdesk-scrape.py
+++ freshdesk-scrape.py
@@ -2,12 +2,16 @@ import requests
 import json
 import argparse
 from datetime import datetime
+import time
 
 parser = argparse.ArgumentParser(description='Tool for gathering and saving information from a Freshdesk instance.')
 parser.add_argument('-k', '--key', help='Freshdesk API key', default='', required=True)
 parser.add_argument('-d', '--domain', help='Specify the Freshdesk domain (ie <domain>.freshdesk.com)', required=True)
 parser.add_argument('-a', '--all', help='Specify if all tickets should be returned (potentially expensive)', required=False, action='store_true')
 parser.add_argument('-r', '--range', help='Specify a range of tickets to be retrieved', type=int, nargs=2)
+parser.add_argument('-l', '--limit', help='Rate limit threshold to pause script execution', type=int, default=1500)
+parser.add_argument('-p', '--pause', help='Pause duration in seconds when rate limit is close', type=int, default=300)  # 300 seconds = 5 minutes
+
 args = parser.parse_args()
 
 # Your Freshdesk domain and API key
@@ -17,6 +21,24 @@ api_key = args.key
 # Basic Authentication
 auth = (api_key, 'X')
 
+# Rate limit handling
+def check_rate_limit(rate_limit_remaining, rate_limit_total, pause_duration):
+    if int(rate_limit_remaining) <= args.limit:
+        print("Approaching rate limit. Pausing execution...")
+        while True:
+            time.sleep(pause_duration)  # Pause for the specified duration
+
+            # Dummy request to check rate limit status
+            response = requests.get(f'https://{domain}.freshdesk.com/api/v2/tickets', auth=auth)
+            new_remaining = int(response.headers.get('X-Ratelimit-Remaining', 0))
+            new_total = int(response.headers.get('X-Ratelimit-Total', 1))
+
+            if new_remaining >= 0.9 * new_total:  # Check if the rate limit has reset to within 90% of the total
+                print("Resuming execution...")
+                break
+            else:
+                print(f"Waiting for rate limit to reset. Current remaining: {new_remaining}")
+
 # Function to fetch tickets
 def fetch_tickets():
     tickets = []
@@ -24,6 +46,14 @@ def fetch_tickets():
     while True:
         url = f'https://{domain}.freshdesk.com/api/v2/tickets?page={page}'
         response = requests.get(url, auth=auth)
+
+        # Checking the rate limit headers
+        rate_limit_remaining = response.headers.get('X-Ratelimit-Remaining', '0')
+        rate_limit_total = response.headers.get('X-Ratelimit-Total', '1')
+
+        # Call the rate limit check function
+        check_rate_limit(rate_limit_remaining, rate_limit_total, args.pause)
+
         data = response.json()
         if data:
             tickets.extend(data)
@@ -40,8 +70,15 @@ def fetch_conversations(ticket_id):
         url = f'https://{domain}.freshdesk.com/api/v2/tickets/{ticket_id}/conversations?page={page}'
         response = requests.get(url, auth=auth)
 
-        rate_limit_total = response.headers.get('X-Ratelimit-Total')
-        rate_limit_remaining = response.headers.get('X-Ratelimit-Remaining')
+        # Existing rate limit header retrieval
+        rate_limit_remaining = response.headers.get('X-Ratelimit-Remaining', '0')
+        rate_limit_total = response.headers.get('X-Ratelimit-Total', '1')
+
+        # Call the rate limit check function
+        check_rate_limit(rate_limit_remaining, rate_limit_total, args.pause)
+
+        #rate_limit_total = response.headers.get('X-Ratelimit-Total')
+        #rate_limit_remaining = response.headers.get('X-Ratelimit-Remaining')
         rate_limit_used_current = response.headers.get('X-Ratelimit-Used-Currentrequest')
 
         print(f"Total Rate Limit: {rate_limit_total}")
